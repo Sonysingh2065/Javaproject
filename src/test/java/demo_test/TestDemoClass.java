cabd04396d2524fc37fcef9fe62c94ffb197037c
sony <sssony1995@gmail.com>
2022-05-04 20:20:44 +0530

    updated commit

--- src/test/java/demo_test/TestDemoClass.java
+++ src/test/java/demo_test/TestDemoClass.java
@@ -10,7 +10,7 @@ public class TestDemoClass {
 	DemoClass obj=new DemoClass();   
 	@Test  
 	public void testSum() {  
-	    assertEquals(4,obj.sum(8,8));  
+	    assertEquals(16,obj.sum(8,8));  
 	         }  
 
 }
